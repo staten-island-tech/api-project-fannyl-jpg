e53f05ad036f7e20debd1301cb2e16c64a31fac3
fannyl-jpg <113190135+fannyl-jpg@users.noreply.github.com>
2023-01-17 10:13:25 -0500

    added async function

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,8 +1,9 @@
 import "./style.css";
 /* import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js' */
+const URL = "https://api.artic.edu/api/v1/artworks";
 
-document.querySelector("#app").innerHTML = `
+/* document.querySelector("#app").innerHTML */ = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -20,8 +21,18 @@ document.querySelector("#app").innerHTML = `
   </div>
 `;
 
-setupCounter(document.querySelector("#counter"));
+/* setupCounter(document.querySelector("#counter")); */
 
 const URL = "https://foodish-api.herokuapp.com/api/dessert";
 
-async function getData(URL) {}
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    document.getElementById("api-response").textContent = data.content;
+  } catch (error) {
+    console.error(error);
+  }
+}
+getData(URL);
+
