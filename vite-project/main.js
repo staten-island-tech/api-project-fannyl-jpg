1a0f845bfded9964a8188a3d81e91c5928a946f7
fannyl-jpg <113190135+fannyl-jpg@users.noreply.github.com>
2023-01-18 10:13:11 -0500

    changed async function

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,9 +1,9 @@
 import "./style.css";
 /* import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js' */
-const URL = "https://api.artic.edu/api/v1/artworks";
+/* const URL = "https://api.artic.edu/api/v1/artworks"; */
 
-/* document.querySelector("#app").innerHTML */ = `
+/* document.querySelector("#app").innerHTML =*/ `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -23,11 +23,14 @@ const URL = "https://api.artic.edu/api/v1/artworks";
 
 /* setupCounter(document.querySelector("#counter")); */
 
-const URL = "https://foodish-api.herokuapp.com/api/dessert";
+const URL = "https://api.artic.edu/api/v1/artworks/search?q=cats";
 
 async function getData(URL) {
   try {
     const response = await fetch(URL);
+    if (response.status < 200 || response.status > 299) {
+      throw new Error(response);
+    }
     const data = await response.json();
     document.getElementById("api-response").textContent = data.content;
   } catch (error) {
@@ -35,4 +38,3 @@ async function getData(URL) {
   }
 }
 getData(URL);
-
