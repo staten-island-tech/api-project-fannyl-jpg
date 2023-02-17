1d146bf23b839639ff49eb25d4f5750481e177b2
fannyl-jpg <113190135+fannyl-jpg@users.noreply.github.com>
2023-02-17 10:11:26 -0500

    added fetch

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,17 +1,37 @@
 import "./styles/style.css";
 
+const apiEntry = " https://api.tvmaze.com/shows/1";
+console.log(fetch(apiEntry));
 const form = document.querySelector(".search-form");
 const imageResults = document.querySelector(".image-container");
+
+fetch(apiEntry)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+async function fetchData(apiEntry) {
+  try {
+    const response = await fetch(apiEntry);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+fetchData(apiEntry);
+
+const apiResponseDOM = document.getElementById("api-response");
+const putQuoteInHTML = async () => {
+  const quote = await fetchData(apiEntry);
+  apiResponseDOM.innerHTML = `Quote: ${quote.content}`;
+};
+putQuoteInHTML();
 
 form.addEventListener("submit", async function (e) {
   e.preventDefault();
 
   const searchTerm = form.elements.query.value;
-
-  const config = { params: { q: searchTerm } };
-  const res = await axios.get(" https://api.tvmaze.com/search/shows", config);
-
-  addImages(res.data);
 
   if (searchTerm === "") {
     const heading = document.createElement("div");
